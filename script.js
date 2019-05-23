8d5c402b04d0e1815502c8ed7074ee42e485efe2
Jesse Silva <thebestjesse76@gmail.com>
2019-05-22 21:44:47 -0600

    so far got script written but unable to use buttons yet

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 
-function myFunction() {
-    document.getElementById("myP")style.fontFamily =
-        "Impact, Charcoal, sans-serif";
-};
+
+// love bob ross and his little trees lol
+function LiteraryTier1() {
+    document.getElementById("one").style.backgroundColor = "blue";
+    document.getElementById("one").style.color = "gold";
+    document.getElementById("one").style.fontFamily = "impact";
+}
+
+//second part of tier 1a
+function LiteraryTier1B() {
+    document.getElementById("two").style.backgroundColor = "red";
+    document.getElementById("two").style.color = "yellow";
+    document.getElementById("two").style.color = "Ariel";
+}
+function LiteraryChallenge() {
+    document.getElementById("three").style.backgroundColor = "black";
+    document.getElementById("three").style.color = "silver";
+    document.getElementById("three").style.fontFamily = "Century Gothic";
+    document.getElementById("three").style.fontWeight = "bolder";
+}
 
